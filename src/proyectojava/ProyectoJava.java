d10a3fbd67931332c6a83f1e0e725369ad60b4cf
dam108 <dam108@A20E926.wirtz.local>
2019-10-18 11:58:27 +0200

    primer commit doval rama

--- src/proyectojava/ProyectoJava.java
+++ src/proyectojava/ProyectoJava.java
@@ -15,7 +15,9 @@ public class ProyectoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        hola hola
+
+// TODO code application logic here
     }
     
 }
